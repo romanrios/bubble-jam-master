af9c9204108056b7f5b01b13d4ce6f3c9589dd7c
Román Ríos <romanrios@live.com>
2025-01-21 14:14:15 -0300

    replace project

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1737479373,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bubble-sheet0.png",
+		"images/particles.png",
+		"media/a5.m4a",
+		"media/a5.ogg",
+		"media/c6.m4a",
+		"media/c6.ogg",
+		"media/d5.m4a",
+		"media/d5.ogg",
+		"media/d6.m4a",
+		"media/d6.ogg",
+		"media/f5.m4a",
+		"media/f5.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
