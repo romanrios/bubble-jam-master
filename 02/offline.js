bd3db9ce5bfc0bd7eea61d3d0216caecc09398db
Román Ríos <romanrios@live.com>
2025-01-23 09:17:46 -0300

    wip

---
+++ 02/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1737634491,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"hello-kookycloud-webfont.woff",
+		"hello-kookycloud-webfont.woff2"
+	]
+}
